f3265de598c9dfc156455efb875b37090b405e44
João Daher <joao@daher.dev>
2021-01-19 18:42:57 -0300

    [Directory] Inherit from DiscoveryMixin

--- gcp_pilot/directory.py
+++ gcp_pilot/directory.py
@@ -1,15 +1,12 @@
 # More Information: https://developers.google.com/admin-sdk/directory/reference/rest
-from typing import Iterator, Dict, Any
+from typing import Dict, Any, Generator
 
-from googleapiclient.errors import HttpError
-
-from gcp_pilot import exceptions
-from gcp_pilot.base import GoogleCloudPilotAPI
+from gcp_pilot.base import GoogleCloudPilotAPI, DiscoveryMixin
 
 UserType = GroupType = MemberType = Dict[str, Any]
 
 
-class Directory(GoogleCloudPilotAPI):
+class Directory(DiscoveryMixin, GoogleCloudPilotAPI):
     _scopes = [
         'https://www.googleapis.com/auth/admin.directory.group',
         'https://www.googleapis.com/auth/admin.directory.user',
@@ -33,7 +30,7 @@ class Directory(GoogleCloudPilotAPI):
             context['domain'] = domain
         return context
 
-    def get_users(self, customer: str = None, domain: str = None) -> Iterator[UserType]:
+    def get_users(self, customer: str = None, domain: str = None) -> Generator[UserType]:
         params = self._build_context(customer=customer, domain=domain)
         yield from self._paginate(
             method=self.client.users().list,
@@ -42,7 +39,7 @@ class Directory(GoogleCloudPilotAPI):
             order_by='email',
         )
 
-    def get_groups(self, customer: str = None, domain: str = None) -> Iterator[GroupType]:
+    def get_groups(self, customer: str = None, domain: str = None) -> Generator[GroupType]:
         params = self._build_context(customer=customer, domain=domain)
         yield from self._paginate(
             method=self.client.groups().list,
@@ -51,9 +48,10 @@ class Directory(GoogleCloudPilotAPI):
         )
 
     def get_group(self, group_id: str) -> GroupType:
-        return self.client.groups().get(
+        return self._execute(
+            method=self.client.groups().get,
             groupKey=group_id,
-        ).execute()
+        )
 
     def create_or_update_group(
             self,
@@ -69,26 +67,23 @@ class Directory(GoogleCloudPilotAPI):
         )
 
         if not group_id:
-            return self.client.groups().insert(
+            return self._execute(
+                method=self.client.groups().insert,
                 body=body,
-            ).execute()
+            )
         else:
-            return self.client.groups().update(
-                groupKey=group_id,
+            return self._execute(
+                method=self.client.groups().update,
                 body=body,
-            ).execute()
+            )
 
     def delete_group(self, group_id: str) -> GroupType:
-        try:
-            return self.client.groups().delete(
-                groupKey=group_id,
-            ).execute()
-        except HttpError as e:
-            if e.resp.status == 404:
-                raise exceptions.NotFoundError()
-            raise e
+        return self._execute(
+            method=self.client.groups().delete,
+            groupKey=group_id,
+        )
 
-    def get_group_members(self, group_id: str) -> Iterator[MemberType]:
+    def get_group_members(self, group_id: str) -> Generator[MemberType]:
         params = dict(
             groupKey=group_id,
         )
@@ -102,27 +97,15 @@ class Directory(GoogleCloudPilotAPI):
     def add_group_member(self, group_id: str, email: str, role: str = 'MEMBER') -> MemberType:
         body = {'email': email, 'role': role}
 
-        try:
-            return self.client.members().insert(
-                groupKey=group_id,
-                body=body,
-            ).execute()
-        except HttpError as e:
-            if e.resp.status == 409:
-                raise exceptions.AlreadyExistsError()
-
-            if e.resp.status == 404:
-                raise exceptions.NotFoundError()
-
-            raise e
+        return self._execute(
+            method=self.client.members().insert,
+            groupKey=group_id,
+            body=body,
+        )
 
     def delete_group_member(self, group_id: str, member_id: str) -> MemberType:
-        try:
-            return self.client.members().delete(
-                groupKey=group_id,
-                memberKey=member_id,
-            ).execute()
-        except HttpError as e:
-            if e.resp.status == 404:
-                raise exceptions.NotFoundError()
-            raise e
+        return self._execute(
+            method=self.client.members().delete,
+            groupKey=group_id,
+            memberKey=member_id,
+        )

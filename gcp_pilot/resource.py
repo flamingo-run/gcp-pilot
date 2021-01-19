96e2ecc5d7881b360d04608bc1fffe1168d91a07
João Daher <joao@daher.dev>
2021-01-19 18:43:11 -0300

    [Resource] Inherit from DiscoveryMixin

--- gcp_pilot/resource.py
+++ gcp_pilot/resource.py
@@ -1,7 +1,7 @@
-from gcp_pilot.base import AccountManagerMixin, GoogleCloudPilotAPI, PolicyType
+from gcp_pilot.base import AccountManagerMixin, GoogleCloudPilotAPI, PolicyType, DiscoveryMixin
 
 
-class GoogleResourceManager(AccountManagerMixin, GoogleCloudPilotAPI):
+class GoogleResourceManager(AccountManagerMixin, DiscoveryMixin, GoogleCloudPilotAPI):
     def __init__(self, **kwargs):
         super().__init__(
             serviceName='cloudresourcemanager',
@@ -11,17 +11,18 @@ class GoogleResourceManager(AccountManagerMixin, GoogleCloudPilotAPI):
         )
 
     def get_policy(self, project_id: str = None, version: int = 1) -> PolicyType:
-        policy = self.client.projects().getIamPolicy(
+        return self._execute(
+            method=self.client.projects().getIamPolicy,
             resource=project_id or self.project_id,
             body={"options": {"requestedPolicyVersion": version}},
-        ).execute()
-        return policy
+        )
 
     def set_policy(self, policy: PolicyType, project_id: str = None) -> PolicyType:
-        return self.client.projects().setIamPolicy(
+        return self._execute(
+            method=self.client.projects().setIamPolicy,
             resource=project_id or self.project_id,
             body={"policy": policy, "updateMask": 'bindings'},
-        ).execute()
+        )
 
     async def add_member(self, email: str, role: str, project_id: str = None) -> PolicyType:
         policy = self.get_policy(project_id=project_id)
@@ -34,6 +35,7 @@ class GoogleResourceManager(AccountManagerMixin, GoogleCloudPilotAPI):
         return self.set_policy(policy=changed_policy, project_id=project_id)
 
     def get_project(self, project_id: str):
-        return self.client.projects().get(
+        return self._execute(
+            method=self.client.projects().get,
             projectId=project_id or self.project_id,
-        ).execute()
+        )

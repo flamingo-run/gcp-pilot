7415c745319c6ca90c08012b3d7fb6d612aadd2c
João Daher <joao@daher.dev>
2021-01-25 12:31:23 -0300

    [PubSub] Add Message tests

--- tests/tests_pubsub.py
+++ tests/tests_pubsub.py
@@ -1,6 +1,7 @@
+import json
 import unittest
 
-from gcp_pilot.pubsub import CloudPublisher, CloudSubscriber  # pylint: disable=unused-import
+from gcp_pilot.pubsub import CloudPublisher, CloudSubscriber, Message  # pylint: disable=unused-import
 
 
 class TestCloudPublisher(unittest.TestCase):
@@ -9,3 +10,38 @@ class TestCloudPublisher(unittest.TestCase):
 
 class TestCloudSubscriber(unittest.TestCase):
     pass
+
+
+class TestPubSubMessage(unittest.TestCase):
+    def _sample_message(self):
+        return {
+            "message": {
+                "attributes": {
+                    "key": "value"
+                },
+                "data": "SGVsbG8gQ2xvdWQgUHViL1N1YiEgSGVyZSBpcyBteSBtZXNzYWdlIQ==",
+                "messageId": "136969346945"
+            },
+            "subscription": "projects/myproject/subscriptions/mysubscription"
+        }
+
+    def assertSampleMessage(self, message):
+        self.assertEqual('136969346945', message.id)
+        self.assertEqual('Hello Cloud Pub/Sub! Here is my message!', message.data)
+        self.assertEqual({"key": "value"}, message.attributes)
+        self.assertEqual("projects/myproject/subscriptions/mysubscription", message.subscription)
+
+    def test_load_from_dict(self):
+        body = self._sample_message()
+        message = Message.load(body=body, parser=str)
+        self.assertSampleMessage(message=message)
+
+    def test_load_from_str(self):
+        body = json.dumps(self._sample_message())
+        message = Message.load(body=body, parser=str)
+        self.assertSampleMessage(message=message)
+
+    def test_load_from_bytes(self):
+        body = json.dumps(self._sample_message()).encode()
+        message = Message.load(body=body, parser=str)
+        self.assertSampleMessage(message=message)

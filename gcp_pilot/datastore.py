c6d76f7f10f9dcbf497bd8f84b9192fcfeefaae6
João Daher <joao@daher.dev>
2021-01-25 12:32:27 -0300

    [Datastore] ORM refactor
    Added .documents manager to read/write models
    Added Meta to store model's metadata

--- gcp_pilot/datastore.py
+++ gcp_pilot/datastore.py
@@ -1,27 +1,21 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, fields
-from typing import Type, Generator, get_type_hints, get_args, Dict, List
+from enum import Enum
+from dataclasses import dataclass
+from typing import Type, Generator, get_args, Dict, ClassVar, Any, Tuple, get_type_hints, Union, Callable
 
 from google.cloud import datastore
 
 from gcp_pilot import exceptions
 
 
-class ClientMixin:
-    _client = None
-
-    @classmethod
-    def _get_client(cls) -> datastore.Client:
-        if not cls._client:
-            cls._client = datastore.Client()
-        return cls._client
+DEFAULT_PK_FIELD = 'id'
 
 
 @dataclass
 class DoesNotExist(Exception):
     cls: Type[EmbeddedDocument]
-    pk: str
+    filters: Dict
 
 
 @dataclass
@@ -30,67 +24,174 @@ class MultipleObjectsFound(Exception):
     filters: Dict
 
 
-operators = {
-    'eq': '=',
-    'gt': '>',
-    'gte': '>=',
-    'lt': '<',
-    'lte': '<=',
-    'in': 'in',
-}
+@dataclass
+class Manager:
+    lookup_operators = {
+        'eq': '=',
+        'gt': '>',
+        'gte': '>=',
+        'lt': '<',
+        'lte': '<=',
+        'in': 'in',
+    }
 
+    _client: ClassVar[datastore.Client] = None
+    fields: Dict[str, type]
+    pk_field: str
+    doc_klass: Type[Document]
+    kind: str
 
-def query_operator(key: str, accepted_fields: List[str]) -> tuple:
-    lookup_fields = []
-    operator = None
+    @classmethod
+    def get_client(cls) -> datastore.Client:
+        if not cls._client:
+            cls._client = datastore.Client()
+        return cls._client
 
-    parts = key.split('__')
-    for idx, part in enumerate(parts):
-        is_last = idx == len(parts) - 1
-        if part in operators:
-            if not is_last:
-                raise exceptions.UnsupportedFormatException(f"Unsupported lookup key format {key}")
-            operator = part
-        elif idx == 0 and part not in accepted_fields:
-            raise exceptions.ValidationError(
-                f"{part} is not a valid field. Excepted one of {' | '.join(accepted_fields)}"
-            )
+    def build_key(self, pk: Any = None) -> datastore.Key:
+        if pk:
+            return self.get_client().key(self.kind, pk)
+        # If no primary key is provided, we let the server create a new ID
+        return self.get_client().allocate_ids(self.get_client().key(self.kind), 1)[0]
+
+    def query(
+            self,
+            distinct_on: str = None,
+            order_by: Union[str, List[str]] = None,
+            page_size: int = None,
+            **kwargs,
+    ) -> datastore.query.Iterator:
+        # base query
+        query = self.get_client().query(kind=self.kind)
+        if order_by:
+            query.order = order_by
+        if distinct_on:
+            query.distinct_on = distinct_on
+
+        # parse lookup args
+        for key, value in kwargs.items():
+            field, operator, value = self._build_filter(key=key, value=value)
+            query.add_filter(field, operator, value)
+
+        # prepare iterator
+        cursor = None
+        empty = False
+        while not empty:
+            query_iter = query.fetch(start_cursor=cursor, limit=page_size)
+            page = next(query_iter.pages)
+            for item in page:
+                yield item
+            cursor = query_iter.next_page_token
+            empty = not bool(cursor)
+
+    def filter(self, **kwargs) -> Generator[Document, None, None]:
+        for entity in self.query(**kwargs):
+            yield self.from_entity(entity=entity)
+
+    def get(self, **kwargs) -> Document:
+        if self.pk_field in kwargs:
+            pk = kwargs[self.pk_field]
+            entity = self.get_client().get(key=self.build_key(pk=pk))
+            if entity:
+                return self.from_entity(entity=entity)
+            raise DoesNotExist(self.doc_klass, pk)
+
+        # Since we can't fetch directly from the key,
+        # we filter and hope for just one object
+        one_obj = None
+        for obj in self.filter(**kwargs):
+            if one_obj is not None:
+                raise MultipleObjectsFound(self.doc_klass, filters=kwargs)
+            one_obj = obj
+        if not one_obj:
+            raise DoesNotExist(self.doc_klass, filters=kwargs)
+        return one_obj
+
+    def create(self, obj: Document) -> Document:
+        entity = self.to_entity(obj=obj)
+        self.get_client().put(entity=entity)
+
+        if not obj.pk:
+            setattr(obj, obj.Meta.pk_field, entity.id)
+        return obj
+
+    def update(self, pk: str, **kwargs) -> Document:
+        if kwargs:
+            entity = self.get_client().get(key=self.build_key(pk=pk))
+            # TODO: enable partial nested updates
+            as_data = {
+                key: value.Meta.to_dict(obj=value) if isinstance(value, EmbeddedDocument) else value
+                for key, value in kwargs.items()
+            }
+            entity.update(as_data)
+            self.get_client().put(entity=entity)
+        return self.get(id=pk)
+
+    def delete(self, pk: str = None):
+        if pk:
+            self.get_client().delete(key=self.build_key(pk=pk))
         else:
-            lookup_fields.append(part)
+            keys = [entity.key for entity in self.query()]
+            self.get_client().delete_multi(keys=keys)
 
-    return '.'.join(lookup_fields), (operator or '=')
+    def _build_filter(self, key: str, value: Any) -> Tuple[str, str, Any]:
+        lookup_fields = []
+        operator = None
+
+        parts = key.split('__') if '__' in key else key.split('.')
+        for idx, part in enumerate(parts):
+            is_last = idx == len(parts) - 1
+            if part in self.lookup_operators:
+                if not is_last:
+                    raise exceptions.UnsupportedFormatException(f"Unsupported lookup key format {key}")
+                operator = part
+            elif idx == 0 and part not in self.fields:
+                raise exceptions.ValidationError(
+                    f"{part} is not a valid field. Excepted one of {' | '.join(self.fields)}"
+                )
+            else:
+                lookup_fields.append(part)
+
+        if isinstance(value, list):
+            raise exceptions.ValidationError(
+                f"Querying with OR clause is not supported"
+            )
+
+        return '.'.join(lookup_fields), (operator or '='), value
+
+    def to_entity(self, obj: Document) -> datastore.Entity:
+        entity = datastore.Entity(key=self.build_key(pk=obj.pk))
+        if not obj.pk:
+            setattr(obj, obj.Meta.pk_field, entity.id)
+        entity.update(obj.Meta.to_dict(obj=obj))
+        return entity
+
+    def from_entity(self, entity: datastore.Entity) -> Document:
+        data = dict(entity.items())
+        if self.pk_field not in data:
+            data[self.pk_field] = entity.id
+        return self.doc_klass.Meta.from_dict(data=data)
 
 
 @dataclass
-class EmbeddedDocument:
-    @classmethod
-    def _fields(cls):
-        resolved_hints = get_type_hints(cls)
-        field_names = [field.name for field in fields(cls)]
-        return {
-            name: resolved_hints[name]
-            for name in field_names
-            if not name.startswith('_')
-        }
+class Metadata:
+    fields: Dict[str, type]
+    doc_klass: Type[EmbeddedDocument]
+    pk_field: str = None
 
-    @classmethod
-    def deserialize(cls, **kwargs) -> EmbeddedDocument:
-        return cls._from_dict(**kwargs)
+    def from_dict(self, data: Dict) -> EmbeddedDocument:
+        data = data.copy()
 
-    @classmethod
-    def _from_dict(cls, **kwargs) -> EmbeddedDocument:
-        data = kwargs.copy()
-
-        def _build(klass, value):
+        def _build(klass: Union[EmbeddedDocument, Callable], value: Any):
             if value is None:
                 return value
 
             if issubclass(klass, EmbeddedDocument):
-                return klass._from_dict(**value)
+                return klass.Meta.from_dict(data=value)
+
             return klass(value)
 
         parsed_data = {}
-        for field_name, field_klass in cls._fields().items():
+        for field_name, field_klass in self.fields.items():
             try:
                 raw_value = data[field_name]
             except KeyError:
@@ -109,23 +210,25 @@ class EmbeddedDocument:
                 item = _build(klass=field_klass, value=raw_value)
             parsed_data[field_name] = item
 
-        return cls(**parsed_data)
+        return self.doc_klass(**parsed_data)
 
-    def serialize(self) -> Dict:
-        return self._to_dict()
-
-    def _to_dict(self) -> dict:
+    def to_dict(self, obj: EmbeddedDocument, select_fields: List[str] = None) -> dict:
         # TODO handle custom dynamic fields
         def _unbuild(value):
             if value is None:
                 return value
             if isinstance(value, EmbeddedDocument):
-                return value._to_dict()
+                return value.Meta.to_dict(obj=value)
+            if isinstance(value, Enum):
+                return value.value
             return value
 
         data = {}
-        for field, field_klass in self.__class__._fields().items():
-            raw_value = getattr(self, field)
+        for field, field_klass in self.fields.items():
+            if select_fields and field not in select_fields:
+                continue
+
+            raw_value = getattr(obj, field)
 
             if getattr(field_klass, '_name', '') == 'List':
                 item = [_unbuild(value=i) for i in raw_value]
@@ -142,87 +245,72 @@ class EmbeddedDocument:
         return data
 
 
+class ORM(type):
+    def __new__(cls, name, bases, attrs):
+        is_abstract_model = name in ['Document', 'EmbeddedDocument']
+        is_concrete_model = 'Document' in [base.__name__ for base in bases]
+
+        if not is_abstract_model:
+            all_fields = cls._extract_fields(attrs=attrs)
+
+            # Since it was not explicitly provided, add id: str = None
+            if DEFAULT_PK_FIELD not in attrs['__annotations__'] and is_concrete_model:
+                attrs['__annotations__'][DEFAULT_PK_FIELD] = int
+                attrs[DEFAULT_PK_FIELD] = None
+
+        new_cls = super().__new__(cls, name, bases, attrs)
+
+        if is_abstract_model:
+            return new_cls
+
+        # Metadata initialization
+        hints = get_type_hints(new_cls)
+        typed_fields = {k: v for k, v in hints.items() if k in all_fields}
+        new_cls.Meta = Metadata(
+            fields=typed_fields,
+            doc_klass=new_cls,
+        )
+
+        # Manager initialization
+        if is_concrete_model:
+            new_cls.documents = Manager(
+                fields=all_fields,
+                pk_field=DEFAULT_PK_FIELD,
+                doc_klass=new_cls,
+                kind=name,
+            )
+
+            new_cls.Meta.pk_field = DEFAULT_PK_FIELD
+
+        return new_cls
+
+    @classmethod
+    def _extract_fields(cls, attrs: Dict) -> Dict[str, type]:
+        return attrs.get('__annotations__', {})
+
+
 @dataclass
-class Document(ClientMixin, EmbeddedDocument):
+class EmbeddedDocument(metaclass=ORM):
+    Meta: ClassVar[Metadata]
+
+    @classmethod
+    def deserialize(cls, **kwargs) -> EmbeddedDocument:
+        return cls.Meta.from_dict(data=kwargs)
+
+    def serialize(self) -> Dict:
+        return self.Meta.to_dict(obj=self)
+
+
+@dataclass
+class Document(EmbeddedDocument):
+    documents: ClassVar[Manager]
+
     @property
-    def pk(self) -> str:
-        raise NotImplementedError()
-
-    @classmethod
-    def _get_key(cls, pk: str = None) -> datastore.Key:
-        return cls._get_client().key(cls._kind(), pk)
-
-    @property
-    def _key(self):
-        return self._get_key(pk=self.pk)
-
-    @classmethod
-    def _kind(cls) -> str:
-        return cls.__name__
-
-    @classmethod
-    def list(cls, **kwargs) -> Generator[Document, None, None]:
-        query = cls._get_client().query(kind=cls._kind())
-
-        for key, value in kwargs.items():
-            field, operator = query_operator(key=key, accepted_fields=list(cls._fields().keys()))
-            query.add_filter(field, operator, value)
-
-        for entity in query.fetch():
-            yield cls._from_dict(**cls.from_entity(entity=entity))
-
-    @classmethod
-    def get(cls, pk: str = None, **kwargs) -> Document:
-        if pk is not None:
-            entity = cls._get_client().get(key=cls._get_key(pk=pk))
-            if entity:
-                return cls._from_dict(**cls.from_entity(entity=entity))
-            raise DoesNotExist(cls, pk)
-
-        one_obj = None
-        for obj in cls.list(**kwargs):
-            if one_obj is not None:
-                raise MultipleObjectsFound(cls, filters=kwargs)
-            one_obj = obj
-        if not one_obj:
-            raise DoesNotExist(cls, pk)
-        return one_obj
-
-    def to_entity(self) -> datastore.Entity:
-        if not self.pk:
-            raise Exception()
-
-        entity = datastore.Entity(key=self._key)
-        entity.update(self._to_dict())
-        return entity
-
-    @classmethod
-    def from_entity(cls, entity: datastore.Entity) -> Dict:
-        return dict(entity.items())
-
-    @classmethod
-    def create(cls, **kwargs) -> Document:
-        data = kwargs.copy()
-        obj = cls.deserialize(**data)
-        return obj.save()
+    def pk(self):
+        return getattr(self, self.Meta.pk_field)
 
     def save(self) -> Document:
-        self._get_client().put(entity=self.to_entity())
-        return self.get(pk=self.pk)
+        return self.documents.create(obj=self)
 
-    @classmethod
-    def update(cls, pk: str, **kwargs) -> Document:
-        if kwargs:
-            entity = cls._get_client().get(key=cls._get_key(pk=pk))
-            # TODO: enable partial nested updates
-            as_data = {
-                key: value._to_dict() if isinstance(value, EmbeddedDocument) else value
-                for key, value in kwargs.items()
-            }
-            entity.update(as_data)
-            cls._get_client().put(entity=entity)
-        return cls.get(pk=pk)
-
-    @classmethod
-    def delete(cls, pk: str) -> None:
-        cls._get_client().delete(key=cls._get_key(pk=pk))
+    def delete(self) -> None:
+        self.documents.delete(pk=self.id)

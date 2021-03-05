8dbeeed2fa0b1d0e338ac5cdffa113b0dd00f0d0
Joao Daher <joao@daher.dev>
2021-03-05 11:09:45 -0300

    [Monitoring] Add new Error Reporting explorer

--- gcp_pilot/error_reporting.py
+++ gcp_pilot/error_reporting.py
@@ -2,7 +2,7 @@
 from google.cloud import error_reporting
 from google.cloud.error_reporting import HTTPContext
 
-from gcp_pilot.base import GoogleCloudPilotAPI
+from gcp_pilot.base import GoogleCloudPilotAPI, DiscoveryMixin
 
 
 class CloudErrorReporting(GoogleCloudPilotAPI):
@@ -40,6 +40,69 @@ class CloudErrorReporting(GoogleCloudPilotAPI):
         )
 
 
+class CloudErrorExplorer(DiscoveryMixin, GoogleCloudPilotAPI):
+    def __init__(self):
+        super().__init__(
+            serviceName='clouderrorreporting',
+            version='v1beta1',
+            cache_discovery=False,
+        )
+
+    def get_events(
+            self,
+            error_id: str = None,
+            service_name: str = None,
+            service_version: str = None,
+            resource_type: str = None,
+            project_id: str = None,
+    ):
+        params = dict(
+            projectName=self._project_path(project_id=project_id),
+            groupId=error_id,
+        )
+        if service_name:
+            params['serviceFilter_service'] = service_name
+        if service_version:
+            params['serviceFilter_version'] = service_version
+        if resource_type:
+            params['serviceFilter_resourceType'] = resource_type
+
+        yield from self._paginate(
+            method=self.client.projects().events().list,
+            params=params,
+            result_key='errorEvents',
+        )
+
+    def get_errors(
+            self,
+            service_name: str = None,
+            service_version: str = None,
+            resource_type: str = None,
+            project_id: str = None,
+    ):
+        params = dict(
+            projectName=self._project_path(project_id=project_id),
+        )
+        if service_name:
+            params['serviceFilter_service'] = service_name
+        if service_version:
+            params['serviceFilter_version'] = service_version
+        if resource_type:
+            params['serviceFilter_resourceType'] = resource_type
+
+        items = self._paginate(
+            method=self.client.projects().groupStats().list,
+            params=params,
+            result_key='errorGroupStats',
+        )
+        for item in items:
+            yield {
+                'id': item['group']['groupId'],
+                **item
+            }
+
+
 __all__ = (
     'CloudErrorReporting',
+    'CloudErrorExplorer',
 )

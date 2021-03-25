d731b1bf1710260a3e09d42316b83200a3f43a22
Joao Daher <joao@daher.dev>
2021-03-25 14:13:31 -0300

    [IAP] Fetch token from metadata server as default

--- gcp_pilot/iap.py
+++ gcp_pilot/iap.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+import requests
 from google.auth.transport.requests import Request
 from google.oauth2 import service_account
 
@@ -7,7 +8,7 @@ from gcp_pilot.base import GoogleCloudPilotAPI
 
 
 class IdentityAwareProxy(GoogleCloudPilotAPI):
-    def __init__(self, audience):
+    def __init__(self, audience: str):
         self.audience = audience
         super().__init__()
 
@@ -25,10 +26,23 @@ class IdentityAwareProxy(GoogleCloudPilotAPI):
             credentials.refresh(request=Request())
         return credentials
 
+    def _get_gcp_token(self):
+        email = self.credentials.service_account_email
+        metadata_server_token_url = f'http://metadata/computeMetadata/v1/instance/service-accounts/{email}/identity'
+
+        token_request_url = f"{metadata_server_token_url}?audience={self.audience}"
+        token_request_headers = {'Metadata-Flavor': 'Google'}
+
+        token_response = requests.get(url=token_request_url, headers=token_request_headers)
+        return token_response.content.decode("utf-8")
+
     @property
     def token(self) -> str:
-        credentials = self._get_id_credentials()
-        return credentials.token
+        try:
+            return self._get_gcp_token()
+        except ConnectionError:
+            credentials = self._get_id_credentials()
+            return credentials.token
 
     @property
     def expires_at(self):

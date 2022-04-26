e8b9d6f9ce25ea31aa0954a7af36577f86f82b6a
João Daher <joao@daher.dev>
2022-04-26 17:27:15 -0300

    refactor: rework Datastore ORM using pydantic

--- gcp_pilot/datastore.py
+++ gcp_pilot/datastore.py
@@ -1,32 +1,32 @@
 from __future__ import annotations
 
-import os
+import abc
 import itertools
+import os
 from collections import defaultdict
-from dataclasses import dataclass, field
-from datetime import datetime
-from enum import Enum
+from dataclasses import dataclass
+from functools import cached_property
 from typing import (
     Type,
     Generator,
-    get_args,
     Dict,
     ClassVar,
     Any,
     Tuple,
-    get_type_hints,
     Union,
     List,
-    Optional,
     Callable,
+    Iterable,
 )
 
 from google.cloud import datastore
+from pydantic import BaseModel
 
 from gcp_pilot import exceptions
 
 DEFAULT_NAMESPACE = os.environ.get("GCP_DATASTORE_NAMESPACE", default=None)
-DEFAULT_PK_FIELD = "id"
+DEFAULT_PK_FIELD_NAME = "id"
+DEFAULT_PK_FIELD_TYPE = int  # pylint: disable=invalid-name
 MAX_ITEMS_PER_OPERATIONS = 500  # Datastore cannot write more than 500 items per call
 
 
@@ -58,7 +58,7 @@ def _starts_with_operator(lookup_fields, value) -> List[Tuple[str, str, Any]]:
 
 @dataclass
 class Manager:
-    lookup_operators: ClassVar[Dict[str, Union[str, Callable]]] = {
+    LOOKUP_OPERATORS: ClassVar[Dict[str, Union[str, Callable]]] = {
         "eq": "=",
         "gt": ">",
         "gte": ">=",
@@ -67,30 +67,37 @@ class Manager:
         "in": "in",
         "startswith": _starts_with_operator,
     }
+    doc_klass: Type[EmbeddedDocument]
 
-    _client: ClassVar[datastore.Client] = None
-    fields: Dict[str, type]
-    pk_field: str
-    doc_klass: Type[Document]
-    kind: str
+    @cached_property
+    def client(self) -> datastore.Client:
+        return datastore.Client(namespace=self.namespace)
 
-    def get_client(self) -> datastore.Client:
-        if not self._client:
-            self._client = datastore.Client(namespace=self.get_namespace())
-        return self._client
+    @property
+    def namespace(self) -> str:
+        return self.doc_klass.__config__.namespace
 
-    def get_namespace(self):
-        return self.doc_klass.Meta.namespace
+    @property
+    def kind(self) -> str:
+        return "EmbeddedKind" if self.is_embedded else self.doc_klass.__name__
 
-    def build_key(self, pk: Any = None, embedded: bool = False) -> datastore.Key:
-        if embedded:
-            return self.get_client().key("EmbeddedKind")
+    @property
+    def is_embedded(self) -> bool:
+        return not issubclass(self.doc_klass, Document)
+
+    @property
+    def fields(self) -> Iterable[str]:
+        return self.doc_klass.__fields__.keys()
+
+    def build_key(self, pk: Any = None) -> datastore.Key:
+        if self.is_embedded:
+            return self.client.key(self.kind)
 
         if pk:
-            typed_pk = self.doc_klass.Meta.fields[self.pk_field](pk)
-            return self.get_client().key(self.kind, typed_pk)
+            typed_pk = DEFAULT_PK_FIELD_TYPE(pk)
+            return self.client.key(self.kind, typed_pk)
         # If no primary key is provided, we let the server create a new ID
-        return self.get_client().allocate_ids(self.get_client().key(self.kind), 1)[0]
+        return self.client.allocate_ids(self.client.key(self.kind), 1)[0]
 
     def _iterate(self, query, page_size):
         cursor = None
@@ -113,7 +120,7 @@ class Manager:
         **kwargs,
     ) -> datastore.query.Iterator:
         # base query
-        query = self.get_client().query(kind=self.kind)
+        query = self.client.query(kind=self.kind)
         if order_by:
             query.order = order_by
         if distinct_on:
@@ -153,14 +160,14 @@ class Manager:
 
     def filter(self, **kwargs) -> Generator[Document, None, None]:
         for entity in self.query(**kwargs):
-            yield self.from_entity(entity=entity)
+            yield self.doc_klass.from_entity(entity=entity)
 
     def get(self, **kwargs) -> Document:
-        if self.pk_field in kwargs:
-            pk = kwargs[self.pk_field]
-            entity = self.get_client().get(key=self.build_key(pk=pk))
+        if DEFAULT_PK_FIELD_NAME in kwargs:
+            pk = kwargs[DEFAULT_PK_FIELD_NAME]
+            entity = self.client.get(key=self.build_key(pk=pk))
             if entity:
-                return self.from_entity(entity=entity)
+                return self.doc_klass.from_entity(entity=entity)
             raise DoesNotExist(self.doc_klass, pk)
 
         # Since we can't fetch directly from the key,
@@ -175,33 +182,32 @@ class Manager:
         return one_obj
 
     def create(self, obj: Document) -> Document:
-        entity = self.to_entity(obj=obj)
-        self.get_client().put(entity=entity)
+        entity = obj.to_entity()
+        self.client.put(entity=entity)
 
         # if successfully saved, we assure the auto-generated ID is added to the final object
         if not obj.pk:
-            setattr(obj, obj.Meta.pk_field, entity.id)
+            setattr(obj, DEFAULT_PK_FIELD_NAME, entity.id)
         return obj
 
     def update(self, pk: str, **kwargs) -> Document:
         if kwargs:
-            entity = self.get_client().get(key=self.build_key(pk=pk))
+            entity = self.client.get(key=self.build_key(pk=pk))
             # TODO: enable partial nested updates
             as_data = {
-                key: value.Meta.to_dict(obj=value) if isinstance(value, EmbeddedDocument) else value
-                for key, value in kwargs.items()
+                key: value.to_dict() if isinstance(value, EmbeddedDocument) else value for key, value in kwargs.items()
             }
             entity.update(as_data)
-            self.get_client().put(entity=entity)
+            self.client.put(entity=entity)
         return self.get(id=pk)
 
     def delete(self, pk: str = None):
         if pk:
-            self.get_client().delete(key=self.build_key(pk=pk))
+            self.client.delete(key=self.build_key(pk=pk))
         else:
             keys = [entity.key for entity in self.query()]
             for chunk in _chunks(keys, MAX_ITEMS_PER_OPERATIONS):
-                self.get_client().delete_multi(keys=chunk)
+                self.client.delete_multi(keys=chunk)
 
     def _build_filter(self, key: str, value: Any) -> List[Tuple[str, str, Any]]:
         operator = None
@@ -209,7 +215,7 @@ class Manager:
         *field_parts, lookup = key.split("__")
         if lookup:
             try:
-                operator = self.lookup_operators[lookup]
+                operator = self.LOOKUP_OPERATORS[lookup]
 
                 if callable(operator):
                     field_name = ".".join(field_parts)
@@ -225,204 +231,65 @@ class Manager:
         field_name = ".".join(field_parts)
         return [(field_name, (operator or "="), value)]
 
-    def to_entity(self, obj: Document) -> datastore.Entity:
-        exclude_from_indexes = obj.Meta.exclude_from_indexes
 
-        if isinstance(obj, Document):
-            entity = datastore.Entity(key=self.build_key(pk=obj.pk), exclude_from_indexes=exclude_from_indexes)
-            if not obj.pk:
-                setattr(obj, obj.Meta.pk_field, entity.id)
-        else:
-            entity = datastore.Entity(key=self.build_key(embedded=True), exclude_from_indexes=exclude_from_indexes)
+class EmbeddedDocument(BaseModel, abc.ABC):
+    class Config:
+        arbitrary_types_allowed = True
+        keep_untouched = (cached_property,)
 
-        entity_data = obj.Meta.to_dict(obj=obj, parse_embedded=self.to_entity)
-        entity.update(entity_data)
-        return entity
+    def __init_subclass__(cls, *args, **kwargs):
+        super().__init_subclass__(*args, **kwargs)
+        cls.documents = Manager(doc_klass=cls)
 
-    def from_entity(self, entity: datastore.Entity) -> Document:
+    @classmethod
+    def from_dict(cls, **kwargs) -> EmbeddedDocument:
+        return cls(**kwargs)
+
+    def to_dict(self) -> Dict:
+        return self.dict()
+
+    @classmethod
+    def from_entity(cls, entity: datastore.Entity) -> EmbeddedDocument:
         data = dict(entity.items())
-        if self.pk_field not in data:
-            data[self.pk_field] = entity.id
-        return self.doc_klass.Meta.from_dict(data=data)
+        data[DEFAULT_PK_FIELD_NAME] = entity.id
+        return cls.from_dict(**data)
 
+    def to_entity(self) -> datastore.Entity:
+        exclude_from_indexes = self.__config__.exclude_from_indexes
 
-@dataclass
-class Metadata:
-    fields: Dict[str, type]
-    doc_klass: Type[EmbeddedDocument]
-    pk_field: str = None
-    namespace: str = DEFAULT_NAMESPACE
-    exclude_from_indexes: List[str] = field(default_factory=list)
-
-    def from_dict(self, data: Dict) -> EmbeddedDocument:
-        data = data.copy()
-
-        def _build(klass: Union[EmbeddedDocument, Callable], value: Any):
-            if value is None or klass == Any:  # pylint: disable=comparison-with-callable
-                return value
-
-            if issubclass(klass, EmbeddedDocument):
-                return klass.Meta.from_dict(data=value)
-
-            if klass == datetime:
-                return klass.fromisoformat(str(value))
-
-            return klass(value)
-
-        parsed_data = {}
-        for field_name, field_klass in self.fields.items():
-            try:
-                raw_value = data[field_name]
-            except KeyError:
-                continue
-
-            if getattr(field_klass, "_name", "") == "List":
-                inner_klass = get_args(field_klass)[0]  # TODO: test composite types
-                item = [_build(klass=inner_klass, value=i) for i in raw_value]
-            elif getattr(field_klass, "_name", "") == "Dict":
-                inner_klass_key, inner_klass_value = get_args(field_klass)
-                item = {
-                    _build(klass=inner_klass_key, value=k): _build(klass=inner_klass_value, value=v)
-                    for k, v in raw_value.items()
-                }
-            else:
-                item = _build(klass=field_klass, value=raw_value)
-            parsed_data[field_name] = item
-
-        return self.doc_klass(**parsed_data)
-
-    def to_dict(
-        self,
-        obj: EmbeddedDocument,
-        select_fields: List[str] = None,
-        parse_embedded: Optional[Callable] = None,
-    ) -> Dict:
-        # TODO handle custom dynamic fields
-        def _unbuild(value):
-            if value is None:
-                return value
-            if isinstance(value, EmbeddedDocument):
-                return parse_embedded(value) if parse_embedded else value.Meta.to_dict(obj=value)
-            if isinstance(value, Enum):
-                return value.value
-            return value
-
-        data = {}
-        for field_name, field_klass in self.fields.items():
-            if select_fields and field not in select_fields:
-                continue
-
-            raw_value = getattr(obj, field_name)
-
-            if getattr(field_klass, "_name", "") == "List":
-                item = [_unbuild(value=i) for i in raw_value]
-            elif getattr(field_klass, "_name", "") == "Dict":
-                item = {_unbuild(value=k): _unbuild(value=v) for k, v in raw_value.items()}
-            else:
-                item = _unbuild(value=raw_value)
-
-            data[field_name] = item
-
-        return data
-
-
-class ORM(type):
-    def __new__(mcs, name, bases, attrs):
-        is_abstract_model = mcs._is_abstract(name=name)
-        is_concrete_model = mcs._is_concrete(bases=bases)
-
-        if not is_abstract_model:
-            # Since it was not explicitly provided, add id: str = None
-            if not mcs._has_explicit_pk_field(attrs=attrs, bases=bases) and is_concrete_model:
-                attrs["__annotations__"][DEFAULT_PK_FIELD] = int
-                attrs[DEFAULT_PK_FIELD] = None
-
-        new_cls = super().__new__(mcs, name, bases, attrs)
-
-        if is_abstract_model:
-            return new_cls
-
-        # Metadata initialization
-        typed_fields = mcs._extract_fields(klass=new_cls)
-        metadata_fields = mcs._extract_fields(klass=new_cls, metadata=True)
-        new_cls.Meta = Metadata(
-            fields=typed_fields,
-            doc_klass=new_cls,
-            namespace=getattr(new_cls, "__namespace__", None),
-            **{field.removeprefix("_"): getattr(new_cls, field) for field in metadata_fields},
-        )
-
-        # Manager initialization
-        if is_concrete_model:
-            new_cls.documents = Manager(
-                fields=typed_fields,
-                pk_field=DEFAULT_PK_FIELD,
-                doc_klass=new_cls,
-                kind=name,
+        if isinstance(self, Document):
+            entity = datastore.Entity(
+                key=self.documents.build_key(pk=self.pk),
+                exclude_from_indexes=exclude_from_indexes,
+            )
+            if not self.pk:
+                setattr(self, DEFAULT_PK_FIELD_NAME, entity.id)
+        else:
+            entity = datastore.Entity(
+                key=self.documents.build_key(),
+                exclude_from_indexes=exclude_from_indexes,
             )
 
-            new_cls.Meta.pk_field = DEFAULT_PK_FIELD
-
-        return new_cls
-
-    @classmethod
-    def _is_abstract(mcs, name: str) -> bool:
-        return name in ["Document", "EmbeddedDocument"]
-
-    @classmethod
-    def _is_concrete(mcs, bases: Tuple[type]) -> bool:
-        return "Document" in [base.__name__ for base in bases]
-
-    @classmethod
-    def _has_explicit_pk_field(mcs, attrs: Dict, bases: Tuple[type]) -> bool:
-        if DEFAULT_PK_FIELD in attrs.get("__annotations__", []):
-            return True
-
-        for base in bases:
-            if DEFAULT_PK_FIELD in get_type_hints(base):
-                return True
-
-        return False
-
-    @classmethod
-    def _extract_fields(mcs, klass: type, metadata: bool = False) -> Dict[str, type]:
-        def _include(var_type: str, k: type):
-            is_private = var_type.startswith("_")
-            is_class_var = (
-                getattr(k, "__origin__", None) == ClassVar or getattr(k, "_name", None) == "ClassVar"
-            )  # pylint: disable=comparison-with-callable
-            return (is_private and is_class_var) if metadata else not (is_private or is_class_var)
-
-        hints = get_type_hints(klass)
-        typed_fields = {var_type: k for var_type, k in hints.items() if _include(var_type, k)}
-        return typed_fields
+        entity.update(self.to_dict())
+        return entity
 
 
-@dataclass
-class EmbeddedDocument(metaclass=ORM):
-    Meta: ClassVar[Metadata]
+class Document(EmbeddedDocument, abc.ABC):
+    id: DEFAULT_PK_FIELD_TYPE
 
-    @classmethod
-    def deserialize(cls, **kwargs) -> EmbeddedDocument:
-        return cls.Meta.from_dict(data=kwargs)
-
-    def serialize(self) -> Dict:
-        return self.Meta.to_dict(obj=self)
-
-
-@dataclass
-class Document(EmbeddedDocument):
-    documents: ClassVar[Manager]
+    class Config(EmbeddedDocument.Config):
+        exclude_from_indexes = ()
+        namespace = DEFAULT_NAMESPACE
 
     @property
     def pk(self):
-        return getattr(self, self.Meta.pk_field, None)
+        return getattr(self, "id", None)
 
     def save(self) -> Document:
         return self.documents.create(obj=self)
 
     def delete(self) -> None:
-        self.documents.delete(pk=self.id)
+        self.documents.delete(pk=self.pk)
 
 
 __all__ = (

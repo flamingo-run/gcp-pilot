7ff04b6ce85338e447967229c16c5b26fa94e0cf
Joao Daher <joao@daher.dev>
2022-08-29 11:53:16 -0300

    feat: add support to manage Users in Google Workspace

--- gcp_pilot/directory.py
+++ gcp_pilot/directory.py
@@ -33,15 +33,6 @@ class Directory(DiscoveryMixin, GoogleCloudPilotAPI):
             context["domain"] = domain
         return context
 
-    def get_users(self, customer: str = None, domain: str = None) -> Generator[UserType, None, None]:
-        params = self._build_context(customer=customer, domain=domain)
-        yield from self._paginate(
-            method=self.client.users().list,
-            result_key="users",
-            params=params,
-            order_by="email",
-        )
-
     def get_groups(self, customer: str = None, domain: str = None) -> Generator[GroupType, None, None]:
         params = self._build_context(customer=customer, domain=domain)
         yield from self._paginate(
@@ -76,7 +67,8 @@ class Directory(DiscoveryMixin, GoogleCloudPilotAPI):
             )
 
         return self._execute(
-            method=self.client.groups().update,
+            method=self.client.groups().patch,
+            groupKey=group_id,
             body=body,
         )
 
@@ -111,6 +103,105 @@ class Directory(DiscoveryMixin, GoogleCloudPilotAPI):
             method=self.client.members().delete,
             groupKey=group_id,
             memberKey=member_id,
+        )
+
+    def get_users(self, customer: str = None, domain: str = None) -> Generator[UserType, None, None]:
+        params = self._build_context(customer=customer, domain=domain)
+        yield from self._paginate(
+            method=self.client.users().list,
+            result_key="users",
+            params=params,
+            order_by="email",
+        )
+
+    def add_user(self, email: str, first_name: str, last_name: str, password: str) -> UserType:
+        body = {
+            "name": {
+                "familyName": last_name,
+                "givenName": first_name,
+            },
+            "password": password,
+            "primaryEmail": email,
+        }
+        # TODO: add support to much more fields from
+        # https://developers.google.com/admin-sdk/directory/reference/rest/v1/users#User
+
+        return self._execute(
+            method=self.client.users().insert,
+            body=body,
+        )
+
+    def update_user(
+        self,
+        user_id: str,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None,
+        password: str = None,
+        suspended: bool = None,
+    ) -> UserType:
+        body = {}
+        if email:
+            body["primaryEmail"] = email
+        if first_name:
+            body["name"]["givenName"] = first_name
+        if last_name:
+            body["name"]["familyName"] = last_name
+        if password:
+            body["password"] = password
+        if suspended is not None:
+            body["suspended"] = suspended
+
+        return self._execute(
+            method=self.client.users().patch,
+            userKey=user_id,
+            body=body,
+        )
+
+    def get_user(self, user_id: str) -> UserType:
+        return self._execute(
+            method=self.client.users().get,
+            userKey=user_id,
+        )
+
+    def delete_user(self, user_id: str) -> UserType:
+        body = {}
+
+        return self._execute(
+            method=self.client.users().delete,
+            userKey=user_id,
+            body=body,
+        )
+
+    def undelete_user(self, user_id: str) -> UserType:
+        body = {}
+
+        return self._execute(
+            method=self.client.users().undelete,
+            userKey=user_id,
+            body=body,
+        )
+
+    def suspend_user(self, user_id: str) -> UserType:
+        return self.update_user(user_id=user_id, suspended=True)
+
+    def reestablish_user(self, user_id: str) -> UserType:
+        return self.update_user(user_id=user_id, suspended=False)
+
+    def make_admin(self, user_id: str) -> UserType:
+        body = {"status": True}
+        return self._execute(
+            method=self.client.users().makeAdmin,
+            userKey=user_id,
+            body=body,
+        )
+
+    def unmake_admin(self, user_id: str) -> UserType:
+        body = {"status": False}
+        return self._execute(
+            method=self.client.users().makeAdmin,
+            userKey=user_id,
+            body=body,
         )
 
 

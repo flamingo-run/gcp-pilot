bc1ba3403fc87f7b17c284753d9b0b1923cc533b
Joao Daher <joao@daher.dev>
2023-01-27 14:02:02 -0300

    feat: add support to structured logs

--- gcp_pilot/logging.py
+++ gcp_pilot/logging.py
@@ -1,7 +1,11 @@
 # More Information: <https://cloud.google.com/logging/docs/reference/libraries#client-libraries-install-python>
+from datetime import datetime
 from logging import INFO
 
 from google.cloud import logging
+from google.cloud.logging_v2 import Resource
+from google.logging.type.http_request_pb2 import HttpRequest
+from google.logging.type.log_severity_pb2 import LogSeverity
 
 from gcp_pilot.base import GoogleCloudPilotAPI
 
@@ -18,6 +22,30 @@ class CloudLogging(GoogleCloudPilotAPI):
     @property
     def handler(self):
         return self.client.get_default_handler()
+
+    def log_struct(
+        self,
+        logger_name: str,
+        message: str | dict,
+        severity: LogSeverity,
+        timestamp: datetime | None = None,
+        http_request: HttpRequest | None = None,
+        labels: dict | None = None,
+        span_id: str | None = None,
+        trace: str | None = None,
+        resource: Resource | None = None,
+    ):
+        logger = self.client.logger(name=logger_name)
+        logger.log_struct(
+            message,
+            timestamp=timestamp or datetime.now(),
+            labels=labels,
+            resource=resource,
+            severity=severity,
+            trace=trace,
+            span_id=span_id,
+            http_request=http_request,
+        )
 
 
 __all__ = ("CloudLogging",)
